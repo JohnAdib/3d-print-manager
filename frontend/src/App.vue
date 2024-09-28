2c848f736a72963a7ea60069427ce456f873d2d9
MrAdib <Mr.John.Adib@gmail.com>
2024-09-28 23:49:26 +0100

    add app

---
+++ frontend/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import Header from '@/components/layout/Header.vue'
+import Footer from '@/components/layout/Footer.vue'
+</script>
+
+<template>
+  <div class="flex flex-col h-screen">
+    <Header />
+    <main class="grow">
+      <RouterView />
+    </main>
+    <Footer />
+  </div>
+</template>
