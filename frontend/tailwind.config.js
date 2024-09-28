871c2f5251048a91f3a24a8b6f1ec3eba31638fa
MrAdib <Mr.John.Adib@gmail.com>
2024-09-29 00:54:18 +0100

    use railway font

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,9 @@
 export default {
   content: ['./src/**/*.{vue,js,ts,jsx,tsx,html}'],
   theme: {
+    fontFamily: {
+      sans: ['Raleway', 'Sans-serif']
+    },
     extend: {}
   },
   plugins: []
