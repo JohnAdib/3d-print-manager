e595c00482647e92119c3e5c62c6acfbf9049b2e
MrAdib <Mr.John.Adib@gmail.com>
2024-09-29 13:51:41 +0100

    save latest changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,14 +7,17 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 4011,
+    watch: {
+      usePolling: true
     }
   }
 })
